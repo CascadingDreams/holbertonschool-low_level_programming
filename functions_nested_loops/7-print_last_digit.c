a650f3f6059077c319f3f7a208b26fff046a8e69
Sammy <myallio94@gmail.com>
2025-02-21 06:17:23 +0000

    task 7

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -11,6 +11,10 @@ int print_last_digit(int r)
 	int last_digit;
 
 	last_digit = r % 10;
+
+	if (last_digit < 0)
+		last_digit = -last_digit;
+
 	_putchar(last_digit + '0');
 	return (last_digit);
 }
