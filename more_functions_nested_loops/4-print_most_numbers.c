b688588c284f71899c406a01574c917fcda8f465
Sammy <myallio94@gmail.com>
2025-02-24 03:54:25 +0000

    task 4

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -9,12 +9,13 @@ void print_most_numbers(void)
 {
 	char c = '0';
 
-	while (c >= '0' && c <= '9' && c++)
+	while (c <= '9')
 	{
 		if (c != '2' && c != '4')
 		{
 			_putchar(c);
 		}
+		c++;
 	}
 	_putchar('\n');
 }
