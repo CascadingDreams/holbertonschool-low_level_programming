6df91be5d970f3e474c1af2d0d1978288ccc6cd0
Sammy <myallio94@gmail.com>
2025-02-26 22:58:29 +0000

    commit to save work

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: 
+ *
+ * Return:
+ */
+int _atoi(char *s)
+{
+	//while char is int, print.
+	//if char is after int, stop.
+}
