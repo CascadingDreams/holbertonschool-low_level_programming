0f7b706e383f91d9a7cf448477179af86a7b35a7
Sammy <myallio94@gmail.com>
2025-02-28 08:48:20 +0000

    task 5

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -2,7 +2,8 @@
 
 /**
  * string_toupper - changes lowercase to uppercase
- * 
+ * @str: string
+ *
  * Return: char
  */
 char *string_toupper(char *str)
@@ -10,7 +11,7 @@ char *string_toupper(char *str)
 	int i = 0;
 
 	while (str[i] != '\0')
-	{	
+	{
 		if (str[i] >= 'a' && str[i] <= 'z')
 		{
 			str[i] = str[i] - 32;
