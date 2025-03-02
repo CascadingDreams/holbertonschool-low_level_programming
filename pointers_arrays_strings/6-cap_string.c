659e98eac4da644ae28a14088a3784846aaf38b4
Sammy <myallio94@gmail.com>
2025-03-02 06:20:49 +0000

    task 6

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -9,6 +9,8 @@
 char *cap_string(char *str)
 {
 	int i = 0;
+	int specialch[13] = {'\n', '\t', ' ', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+	int j = 0;
 
 	while (str[i] != '\0')
 	{
@@ -16,10 +18,14 @@ char *cap_string(char *str)
 		{
 			str[i] = str[i] - 32;
 		}
-		if (str[i] == '\n' || str[i] == '\t' || str[i] == ' ' || str[i] == ',' || str[i] == ';' || str[i] == '.' || str[i] == '!' || str[i] == '?' || str[i] == '"' || str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}')
+		j = 0;
+		while (specialch[j] != '\0')
 		{
-			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+			if (str[i] == specialch[j] && str[i + 1] >= 'a' && str[i + 1] <= 'z')
+			{
 				str[i + 1] = str[i + 1] - 32;
+			}
+			j++;
 		}
 		i++;
 	}
