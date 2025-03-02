95d7b2b5c3636241e6e36015b0b6ddb4dae48d86
Sammy <myallio94@gmail.com>
2025-03-02 05:15:18 +0000

    task 6 update

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -12,6 +12,10 @@ char *cap_string(char *str)
 
 	while (str[i] != '\0')
 	{
+		if (i == 0 && str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+		}
 		if (str[i] == '\n' || str[i] == '\t' || str[i] == ' ' || str[i] == ',' || str[i] == ';' || str[i] == '.' || str[i] == '!' || str[i] == '?' || str[i] == '"' || str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}')
 		{
 			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
