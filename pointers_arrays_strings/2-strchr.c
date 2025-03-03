783f7f50a42d8409790c7a3452b5b79ddaeeb5a0
Sammy <myallio94@gmail.com>
2025-03-03 10:21:31 +0000

    task 2

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _strchr - locates a character in a string
@@ -11,13 +12,17 @@ char *_strchr(char *s, char c)
 {
 	int i = 0;
 
+	if (c == '\0')
+	{
+		return (NULL);
+	}
 	while (s[i] != '\0')
 	{
 		if (s[i] == c)
 		{
-			return (&s[i]);
+			return (s + i);
 		}
 		i++;
 	}
-	return ('\0');
+	return (NULL);
 }
