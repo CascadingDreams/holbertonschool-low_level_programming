b8a127e0af08b68ae5780e88e33c0016c84a1f67
Sammy <myallio94@gmail.com>
2025-03-04 05:59:06 +0000

    commit to save

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *
+ *
+ *
+ *
+ */
+void print_diagsums(int *a, int size)
+{
+	int rowCount = 0;
+	int colCount = 0;
+
+	while (a[rowCount] < size)
+	{
+		rowCount++;
+		while (a[colCount] < size)
+		{
+			//colCount++;
+		}
+		if (a[rowCount] == a[colCount])
+		{
+			sum = 
+			sum++;
+		}
+	}
+
+			// primary diagonal 
+			// if row == col
+			// r == n - 1
+	
+
+}
