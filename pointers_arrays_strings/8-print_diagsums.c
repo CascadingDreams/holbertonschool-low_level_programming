421f21e4c662d6dbdc841bac34bdf9fab409deae
Sammy <myallio94@gmail.com>
2025-03-05 03:38:06 +0000

    task 7

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -2,33 +2,23 @@
 #include <stdio.h>
 
 /**
+ * print_diagsums - Prints the sum of two diagonals in a matrix.
+ * @a : pointer to matrix
+ * @size: size of square matrix
  *
- *
- *
- *
+ * Return: void
  */
 void print_diagsums(int *a, int size)
 {
-	int rowCount = 0;
-	int colCount = 0;
+	int count = 0;
+	int p_sum = 0;
+	int s_sum = 0;
 
-	while (a[rowCount] < size)
+	while (count < size)
 	{
-		rowCount++;
-		while (a[colCount] < size)
-		{
-			//colCount++;
-		}
-		if (a[rowCount] == a[colCount])
-		{
-			sum = 
-			sum++;
-		}
+		p_sum = p_sum + a[count * size + count];
+		s_sum = s_sum + a[count * size + (size - 1 - count)];
+		count++;
 	}
-
-			// primary diagonal 
-			// if row == col
-			// r == n - 1
-	
-
+	printf("%d, %d\n", p_sum, s_sum);
 }
