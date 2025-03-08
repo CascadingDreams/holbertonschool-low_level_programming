2de5fbe67d597076c5d1bf3a51711a14d5c5486f
Sammy <myallio94@gmail.com>
2025-03-08 01:58:02 +0000

    task 0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: count
+ * @argv: vector
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	if (argc == 1)
+	printf("%s\n", *argv);
+	return (0);
+}
