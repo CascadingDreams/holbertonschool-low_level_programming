62ce350e7e6ba539187008c2dae8882c623ad227
Sammy <myallio94@gmail.com>
2025-03-16 01:56:21 +0000

    task 0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+#include <stdlib.h>
+
+/**
+ * struct dog - info about a dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Description: this holds info about a dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
