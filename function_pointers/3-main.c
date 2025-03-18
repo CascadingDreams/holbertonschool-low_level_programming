2f0a65d820640e4a0b1706acd0ca5c44f432b782
Sammy <myallio94@gmail.com>
2025-03-18 05:00:51 +0000

    task 3

---
+++ function_pointers/3-main.c
@@ -0,0 +1,36 @@
+#include "3-calc.h"
+
+/**
+ * main - entry
+ * @argc: count
+ * @argv: vector
+ *
+ * description: calculator
+ * Return: 0 success
+ */
+int main(int argc, char *argv[])
+{
+	int num1;
+	int num2;
+	int result;
+	int (*operator)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	operator = get_op_func(argv[2]);
+
+	if (operator == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	result = operator(num1, num2);
+
+	printf("%d\n", result);
+	return (0);
+}
---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,71 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds two integers
+ * @a: num1
+ * @b: num2
+ *
+ * Return: sum of a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - substracts two ints
+ * @a: num1
+ * @b: num2
+ *
+ * Return: difference between a and b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies two integers
+ * @a: num1
+ * @b: num2
+ *
+ * Return: product of a and b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides two integers
+ * @a: num1
+ * @b: num2
+ *
+ * Return: division of and b
+ */
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		return (100);
+	}
+	return (a / b);
+}
+
+/**
+ * op_mod - modulo a and b
+ * @a: num1
+ * @b: num2
+ *
+ * Return: remainder of the division of a and b
+ */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		return (100);
+	}
+		return (a % b);
+}
